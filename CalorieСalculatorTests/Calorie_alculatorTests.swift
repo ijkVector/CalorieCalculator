88fa0338fbac340cea204ec28f55e4c53cffc882
ijkVector <johnsmith28122000@gmail.com>
2025-11-23 15:01:36 +0300

    Initial Commit

---
+++ CalorieСalculatorTests/Calorie_alculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_alculatorTests.swift
+//  CalorieСalculatorTests
+//
+//  Created by Иван Дроботов on 11/23/25.
+//
+
+import Testing
+@testable import Calorie_alculator
+
+struct Calorie_alculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ CalorieСalculatorUITests/Calorie_alculatorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Calorie_alculatorUITestsLaunchTests.swift
+//  CalorieСalculatorUITests
+//
+//  Created by Иван Дроботов on 11/23/25.
+//
+
+import XCTest
+
+final class Calorie_alculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
